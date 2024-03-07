ab085dc7e5fe21398426145bc2d1a8eed328e5cb
Jayas1508 <162321772+Jayas1508@users.noreply.github.com>
2024-03-07 15:57:09 +0530

    Add files via upload

---
+++ l-1/home.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+main()
+{
+	printf("Name = Jay asodariya\n");
+	printf("Age = 23\n");
+	printf("Collage = vnsgu \n");
+	
+	
+	printf("\n**********************************\n\t");
+	
+	
+	
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*     * *       *\n");
+	printf("*    *   *     *\n");
+	printf("*   *     *   *\n");
+	printf("*  *       * *\n");
+	printf("* *\n");
+	printf("*\n");
+	
+	
+	printf("\n**********************************\n");
+	
+	
+	
+	printf(" --------\n");
+	printf("|\t |\n");
+	printf("R\t |\n");
+	printf("N\t |\n");
+	printf("W\t |\n");
+	printf("|\t |\n");
+	printf(" --------\n");
+	
+	
+    printf("\n**********************************\n");
+	
+	
+	printf("*\n");
+	printf("* *\n");
+	printf("* * *\n");
+	printf("* *\n");
+	printf("*\n");
+	
+}
