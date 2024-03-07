d2daeac5acb733444c20d338a3c0239e43b2f429
Jayas1508 <162321772+Jayas1508@users.noreply.github.com>
2024-03-07 17:47:00 +0530

    Add files via upload

---
+++ l-7/if-else.c.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int a, b;
+	
+	printf("Enter value of A = ");
+	scanf("%d",&a);
+	
+	printf("Enter value of B = ");
+	scanf("%d",&b);
+	
+	if(a<=b)
+	{
+		printf("A is min");
+	}
+	else
+	{
+		printf("B is min");
+	}
+	
+}
